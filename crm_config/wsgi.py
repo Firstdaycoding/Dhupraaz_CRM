1840738e386fb84e2446188c9b7124e3c911015a
Kanhaiya <153911666+Firstdaycoding@users.noreply.github.com>
2026-07-24 03:10:54 +0530

    wsgi settings updated

--- crm_config/wsgi.py
+++ crm_config/wsgi.py
@@ -8,10 +8,15 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Path to your project folder where manage.py is located
+path = '/home/yourusername/your-repo-name'
+if path not in sys.path:
+    sys.path.append(path)
+
+# Name of the folder containing settings.py
+os.environ['DJANGO_SETTINGS_MODULE'] = 'your_project_name.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crm_config.settings')
-
 application = get_wsgi_application()
-app = application
